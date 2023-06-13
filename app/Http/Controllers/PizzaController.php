cb3db4127032eaf1a1dca205fd58480a2c3d1080
Giorgio22 <giorgioprizzi@gmail.com>
2023-06-13 17:44:10 +0200

    Push: bvb

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
-class PizzaController extends Controller
-{
-    // public function index(){
+class PizzaController extends Controller{
 
-    // }
+    public function index(){
+        $pizzas = Pizza::All();
+        return view('welcome', compact('pizzas'));
+    }
 }
+
+
