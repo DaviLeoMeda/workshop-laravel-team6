3539561c118a7a50202e6583d74aa832aeb172bf
DaviLeoMeda <davileo.meda@gmail.com>
2023-06-13 17:27:05 +0200

    Sixth Step - pushed on DB

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
-    // public function index(){
+    public function index(){
+        $pizzas = Pizza::All();
 
-    // }
+        return view('welcome', compact('pizzas'));
+    }
 }
--- database/migrations/2023_06_13_140612_create_pizzas_table.php
+++ database/migrations/2023_06_13_140612_create_pizzas_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Pizza;
 
 return new class extends Migration
 {
@@ -15,6 +16,10 @@ return new class extends Migration
     {
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome_pizza');
+            $table->string('ingredienti');
+            $table->decimal(4,2);
+            $table->boolean('allergeni')->default(false);
             $table->timestamps();
         });
     }
